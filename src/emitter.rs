b9eb7ed65016c39b62fc48f14d886dfb2c05a143
Cheng JIANG <jiang.cheng@vip.163.com>
2020-04-06 14:20:10 +0200

    Fix CI.

--- src/emitter.rs
+++ src/emitter.rs
@@ -13,7 +13,7 @@ pub(crate) enum Event {
 
 lazy_static! {
     pub(crate) static ref EMITTER: Mutex<Emitter<'static, Event, Enforcer>> =
-        { Mutex::new(Emitter::new()) };
+        Mutex::new(Emitter::new());
     pub(crate) static ref CACHED_EMITTER: Mutex<Emitter<'static, Event, CachedEnforcer>> =
-        { Mutex::new(Emitter::new()) };
+        Mutex::new(Emitter::new());
 }
