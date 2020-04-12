c3ab3553f95abb5c43d55293392a482effd78157
Cheng JIANG <jiang.cheng@vip.163.com>
2020-04-12 09:59:47 +0200

    Fix CI warnings

--- src/enforcer.rs
+++ src/enforcer.rs
@@ -71,7 +71,7 @@ pub struct Enforcer {
 
 impl EventEmitter<Event> for Enforcer {
     fn on(&mut self, e: Event, f: fn(&mut Self, Option<EventData>)) {
-        self.events.entry(e).or_insert_with(|| Vec::new()).push(f)
+        self.events.entry(e).or_insert_with(Vec::new).push(f)
     }
 
     fn off(&mut self, e: Event) {
