588f0c566d7adb26596a3e9155ed05072772dac0
Cheng JIANG <jiang.cheng@vip.163.com>
2020-05-21 15:32:06 +0200

    remove explain relevant code when feature has been disabled

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -9,6 +9,7 @@ pub trait Logger: Send + Sync {
     fn is_enabled(&self) -> bool;
     fn print_enforce_log(&self, rvals: Vec<String>, authorized: bool, cached: bool);
     fn print_mgmt_log(&self, d: &EventData);
+    #[cfg(feature = "explain")]
     fn print_expl_log(&self, rules: Vec<&Vec<String>>);
     fn print_status_log(&self, enabled: bool);
 }
