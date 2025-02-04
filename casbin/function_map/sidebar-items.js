d65f0eed104958395e777b735a1d7306cd70c264
hsluoyz <hsluoyz@users.noreply.github.com>
2025-02-04 09:55:15 +0000

    deploy: b6b3be137c0e60dea224d401350eb4f6d5669827

--- casbin/function_map/sidebar-items.js
+++ casbin/function_map/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["OperatorFunction"],"fn":["key_get","key_get2","key_get3","key_match","key_match2","key_match3","regex_match"],"struct":["FunctionMap"]};
+window.SIDEBAR_ITEMS = {"enum":["OperatorFunction"],"fn":["key_get","key_get2","key_get3","key_match","key_match2","key_match3","key_match4","key_match5","regex_match"],"struct":["FunctionMap"]};
