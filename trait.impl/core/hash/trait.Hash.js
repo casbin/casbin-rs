b232937803c1edf8caf58b2ace7012ac01ec6d29
hsluoyz <hsluoyz@users.noreply.github.com>
2025-09-17 10:21:43 +0000

    deploy: 33c165a3d30727dfd87c8c5363c7133c8e5d3ca2

--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["casbin",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"casbin/enum.Event.html\" title=\"enum casbin::Event\">Event</a>"]]]]);
+    var implementors = Object.fromEntries([["casbin",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"casbin/enum.Event.html\" title=\"enum casbin::Event\">Event</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
